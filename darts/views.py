cc3989346e326e1c2d591f0c56eb21df98c224ec
Hiroaki Honda <manybooks.0703@gmail.com>
2016-10-03 07:58:42 +0900

    fix the bug that can't get query string in the source url

--- darts/views.py
+++ darts/views.py
@@ -7,19 +7,29 @@ from .parser import HtmlParser, TextParser
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'client_secrets.json'), 'r') as f:
     secrets = json.load(f)
-    GOOGLE_MAPS_JS_API_KEY = secrets['key']['js']
+    GOOGLE_MAPS_API_KEY = secrets['key']['g_map_api']
 
 GOOGLE_MAPS_URL_BASE = 'https://maps.googleapis.com/maps/api/js?'
 CALLBACK_FUNCTION = 'initMap'
-MAP_SRC = GOOGLE_MAPS_URL_BASE + urlencode({'key': GOOGLE_MAPS_JS_API_KEY, 'callback': CALLBACK_FUNCTION, 'signed_in': 'true'})
+MAP_SRC = GOOGLE_MAPS_URL_BASE + urlencode({'key': GOOGLE_MAPS_API_KEY, 'callback': CALLBACK_FUNCTION, 'signed_in': 'true'})
 
 def index(request):
     context = { 'map_src': MAP_SRC, }
     return render(request, 'darts/index.html', context)
 
 def from_url(request, url):
+    url += _get_query_in_src_url(request)
     parser = HtmlParser(url=url)
     addresses = parser.make_darts()
     context = { 'map_src': MAP_SRC,
                 'addresses': addresses}
     return render(request, 'darts/from_url.html', context)
+
+def _get_query_in_src_url(request):
+    """if source url contains query, WSGI recognize that as 'QUERY_STRING' of META information and
+    remove from 'url' keyword argument of from_url function. So this function salvage and return
+    the query part of source url.""" 
+    query = request.META["QUERY_STRING"]
+    if query:
+        return "?" + query
+    return ""
